74361421483fc009e3e09690a1f306a34c124836
iamsid47 <siddheshkulthe43@gmail.com>
2023-08-10 23:30:38 +0530

    add job description handle, spacy ner update

--- app.py
+++ app.py
@@ -6,9 +6,18 @@ import pdfplumber
 from rank_match import rank_cvs_with_transformers
 from rank_match import rank_cvs_with_spacy_similarity
 import logging
+import io
+from pdfplumber import open as pdf_open
 
 app = Flask(__name__)
 CORS(app)
+
+custom_nlp = spacy.load('D:/CVNER/archive/output/model-last/')
+
+# Load English tokenizer, POS tagger, parser, NER, and word vectors
+nlp = spacy.load('en_core_web_trf', disable=['parser', 'ner'])
+nlp_trf = spacy.load('en_core_web_trf', disable=['parser', 'textcat'])
+
 
 def pdf_to_text(pdf_path):
     txt_path = f"uploads/{os.path.splitext(os.path.basename(pdf_path))[0]}.txt"
@@ -24,21 +33,43 @@ def pdf_to_text(pdf_path):
     return txt_path
 
 def process_cv_ner(cv_text):
-    model_path = "D:/CVNER/archive/output/model-last/"
-    nlp = spacy.load(model_path)
-    doc = nlp(cv_text)
+    preprocessed_cv_text = preprocess_text(cv_text)  # Preprocess with en_core_web_trf
+    doc = custom_nlp(preprocessed_cv_text)  # Use your custom NER model
     cv_ner_outputs = []
     for ent in doc.ents:
-        cv_ner_outputs.append([ent.text])
+        cv_ner_outputs.append([ent.text, ent.label_])  # Extract only entity text
     return cv_ner_outputs
+
+def preprocess_text(text):
+    doc = nlp(text)
+    clean_tokens = []
+    for token in doc:
+        if not token.is_stop and not token.is_punct:
+            clean_tokens.append(token.lemma_)
+    return ' '.join(clean_tokens)
+
+def process_job_description_ner(job_description):
+    doc = nlp_trf(job_description)
+    job_desc_ner_outputs = []
+    for ent in doc.ents:
+        job_desc_ner_outputs.append([ent.text, ent.label_])
+    return job_desc_ner_outputs
 
 @app.route('/process_cvs', methods=['POST'])
 def process_cvs():
     try:
         uploaded_files = request.files.getlist('files')
-        job_description = request.form.get('jobDescription')
+        job_description_file = request.files['jobDescriptionFile']
+        job_description_text = ""
+        with pdf_open(job_description_file) as pdf:
+            for page in pdf.pages:
+                job_description_text += page.extract_text()
+        
+        job_description_ner_outputs = process_job_description_ner(job_description_text)
+        job_description_ner_text = ' '.join([entity_text for entity_text, _ in job_description_ner_outputs])
+
         cv_ner_outputs_list = []
-        cv_text_list = []
+        cv_text_list = []  
 
         for uploaded_file in uploaded_files:
             pdf_path = f"uploads/{uploaded_file.filename}"
@@ -50,25 +81,27 @@ def process_cvs():
             
             with open(txt_path, "r", encoding='utf-8') as f:
                 cv_text = f.read()  
-                cv_text_list.append(cv_text)    
-            
-            # cv_ner_outputs = []
-            # for line in cv_text_lines:
-            #     processed_line_ner = process_cv_ner(line)
-            #     cv_ner_outputs.extend(processed_line_ner)
-            # cv_ner_outputs_list.append(cv_ner_outputs)
-            cv_ner_outputs = process_cv_ner(cv_text)
-            cv_ner_outputs_list.append(cv_ner_outputs)
+                cv_ner_outputs = process_cv_ner(cv_text)
+                cv_ner_outputs_list.append(cv_ner_outputs)
 
         app.logger.info('Files uploaded successfully')
 
-        ranked_cvs = rank_cvs_with_spacy_similarity(job_description, cv_text_list)
+        # ranked_cvs = rank_cvs_with_spacy_similarity(job_description_ner_text, cv_ner_outputs_list)
+        # ranked_cv_data = []
+
+
+        for idx, cv in enumerate(cv_ner_outputs_list):
+            cv_text = ' '.join([entity_text for entity_text, _ in cv])
+            cv_text_list.append(cv_text)  # Append CV text to the list
+            print(f"CV {idx} Entity Texts:", cv_text)
+        ranked_cvs = rank_cvs_with_spacy_similarity(job_description_ner_text, cv_text_list)  # Use job_description_ner_text
+
         ranked_cv_data = []
+
 
         for rank, (cv_idx, score) in enumerate(ranked_cvs, start=1):  
             ranked_cv_data.append({
                 "rank": rank,
-                # "cv_idx": cv_idx, 
                 "score": float(score * 100),
                 "file_name": uploaded_files[cv_idx].filename,
                 "ner_output": cv_ner_outputs_list[cv_idx]  
@@ -79,8 +112,6 @@ def process_cvs():
     except Exception as e:
         app.logger.error(str(e))
         return jsonify({'error': str(e)}), 500
-
-
 
 if __name__ == '__main__':
     os.makedirs("uploads", exist_ok=True)
--- rank_match.py
+++ rank_match.py
@@ -18,18 +18,48 @@ def rank_cvs_with_transformers(job_description, cv_ner_outputs_list):
     ranked_cvs = sorted(ranked_cvs, key=lambda x: x[1], reverse=True)
     return ranked_cvs
 
+# def rank_cvs_with_spacy_similarity(job_description, cv_text_list):
+#     nlp = spacy.load('en_core_web_lg')
+
+#     jd_doc = nlp(job_description)
+
+#     ranked_cvs = []
+#     for idx, cv_text in enumerate(cv_text_list):
+#         cv_doc = nlp(cv_text)
+
+#         similarity_score = cv_doc.similarity(jd_doc)
+#         ranked_cvs.append((idx, similarity_score))
+
+#     ranked_cvs = sorted(ranked_cvs, key=lambda x: x[1], reverse=True)
+#     return ranked_cvs
+
+# def rank_cvs_with_spacy_similarity(job_description, cv_ner_outputs_list):
+#     nlp = spacy.load('en_core_web_lg')
+
+#     job_description_doc = nlp(job_description)
+
+#     ranked_cvs = []
+#     for idx, cv in enumerate(cv_ner_outputs_list):
+#         cv_text = ' '.join([entity_text for entity_text, _ in cv])  # Join only entity text
+#         cv_doc = nlp(cv_text)
+
+#         similarity_score = cv_doc.similarity(job_description_doc)
+#         ranked_cvs.append((idx, similarity_score))
+
+#     ranked_cvs = sorted(ranked_cvs, key=lambda x: x[1], reverse=True)
+#     return ranked_cvs
+
 def rank_cvs_with_spacy_similarity(job_description, cv_text_list):
     nlp = spacy.load('en_core_web_lg')
 
-    jd_doc = nlp(job_description)
+    job_description_doc = nlp(job_description)
 
     ranked_cvs = []
     for idx, cv_text in enumerate(cv_text_list):
         cv_doc = nlp(cv_text)
 
-        similarity_score = jd_doc.similarity(cv_doc)
+        similarity_score = cv_doc.similarity(job_description_doc)
         ranked_cvs.append((idx, similarity_score))
 
     ranked_cvs = sorted(ranked_cvs, key=lambda x: x[1], reverse=True)
     return ranked_cvs
-
